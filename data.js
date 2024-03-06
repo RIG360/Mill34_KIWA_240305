a226e7e76c2d957b87bf5b091ef8b4d834295098
RIG360 <office@rimpfl.at>
2024-03-06 14:11:21 +0100

    upload

---
+++ data.js
@@ -0,0 +1,189 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kiwa-1",
+      "name": "KIWA (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -3.035990529099667,
+        "pitch": 0.11022858255989121,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.048855734930388,
+          "pitch": 0.5283797399273418,
+          "rotation": 0,
+          "target": "1-kiwa-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.461065067507496,
+          "pitch": -0.4269879196830324,
+          "title": "Parteienkeller",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-kiwa-2",
+      "name": "KIWA (2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -3.1078261819983055,
+        "pitch": 0.004814801294974913,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.010323230638267233,
+          "pitch": 0.5312637581846857,
+          "rotation": 0,
+          "target": "0-kiwa-1"
+        },
+        {
+          "yaw": -1.621247705157753,
+          "pitch": 0.6358462105106071,
+          "rotation": 0,
+          "target": "2-kiwa-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6681022516815798,
+          "pitch": -0.4364750225987848,
+          "title": "Parteienkeller",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "2-kiwa-3",
+      "name": "KIWA (3)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -1.5814923017830615,
+        "pitch": 0.02044195589568787,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.108195441015326,
+          "pitch": 0.4351707299438061,
+          "rotation": 0,
+          "target": "3-kiwa-4"
+        },
+        {
+          "yaw": 1.5724491960694564,
+          "pitch": 0.6438038037158016,
+          "rotation": 0,
+          "target": "1-kiwa-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-kiwa-4",
+      "name": "KIWA (4)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -2.8855690310832003,
+        "pitch": -0.006883477707395613,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.17512297153316325,
+          "pitch": 0.4651141964526495,
+          "rotation": 0,
+          "target": "2-kiwa-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Mill34, KIWA_240305",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
